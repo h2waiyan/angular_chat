72458a818dd6d178430a7a325dc4470d0492237d
Htoo Wai Yan <h2wy.mog@gmail.com>
2022-12-08 11:50:44 +0630

    socket connection added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,26 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:5000");
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
-
   messages: any[] = [];
-
   constructor() {
-  }
+    socket.on("connection", () => {
+      console.log(socket.id); // x8WIv7-mJelg7on_ALbx
 
+    });
+    socket.on("message", (msg) => {
+      console.log(msg)
+    })
+  }
 
   ngOnInit(): void {
   }
 
-  sendMessage(){
-
+  sendMessage() {
+    socket.emit("message", "Hello")
   }
-
-  updateTextMessage(event : any){
-
+  
+  updateTextMessage(event: any) {
   }
 }
